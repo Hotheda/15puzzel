7659593a19e2a8dc2053168254d5dc940dee0646
Heda <hotheda@gmail.com>
2020-09-29 20:50:57 +0200

    Updates

---
+++ precache-manifest.3b1c2ca48f6a13fe14f05dd86fdff200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab487f2d2ee3fc05199f4876a0ef225",
+    "url": "/15puzzel/index.html"
+  },
+  {
+    "revision": "01263837a404edc09f8e",
+    "url": "/15puzzel/static/css/main.db2a11c1.chunk.css"
+  },
+  {
+    "revision": "6a93078a656621bf3d84",
+    "url": "/15puzzel/static/js/2.3b1925df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/15puzzel/static/js/2.3b1925df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01263837a404edc09f8e",
+    "url": "/15puzzel/static/js/main.f35812d5.chunk.js"
+  },
+  {
+    "revision": "0515d7fd07ed6ce2a823",
+    "url": "/15puzzel/static/js/runtime-main.aae5ccd7.js"
+  }
+]);
