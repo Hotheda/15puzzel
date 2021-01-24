bb50d76f4ff3d41deb9bf3a83b5dd50d5da1876d
Heda <hotheda@gmail.com>
2021-01-24 12:26:55 +0100

    css images

--- src/Components/Winner.js
+++ src/Components/Winner.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Winner(props){
     return(
         <div className={"winner_text"}>
-            <h1>You win, gz!</h1>
+            <h1>You win, congratulations!</h1>
             <p>It took {props.numberOfMoves} moves to finish</p>
         </div>
     )
